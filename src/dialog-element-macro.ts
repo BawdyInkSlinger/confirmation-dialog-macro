f342550867bd396cc7330ef1f866f0ec07e1881d
Bawdy Ink Slinger <BawdyInkSlinger@gmail.com>
2023-06-29 21:10:05 -0700

    A failing test

--- src/dialog-element-macro.ts
+++ src/dialog-element-macro.ts
@@ -1,7 +1,44 @@
 Macro.add("dialogelement", {
   tags: ["onopen", "onclose"],
   handler: function () {
-    // todo
+    let content = "";
+    let onOpen: string | null = null;
+    let onClose: string | null = null;
+    const title = this.args.length > 0 ? this.args[0] : "";
+    const classes = this.args.length > 1 ? this.args.slice(1).flatten() : [];
+
+    this.payload.forEach(function (payload, idx) {
+      if (idx === 0) {
+        content = payload.contents;
+      } else {
+        if (payload.name === "onopen") {
+          onOpen = onOpen ? onOpen + payload.contents : payload.contents;
+        } else {
+          onClose = onClose ? onClose + payload.contents : payload.contents;
+        }
+      }
+    });
+
+    classes.push("macro-" + this.name);
+
+    Dialog.setup(title, classes.join(" "));
+    Dialog.wiki(content);
+
+    if (onOpen !== null && typeof onOpen === "string") {
+      const onOpenString: string = onOpen;
+      $(document).one(":dialogopened", function () {
+        $.wiki(onOpenString.trim());
+      });
+    }
+
+    if (onClose !== undefined && typeof onClose === "string") {
+      const onCloseString: string = onClose;
+      $(document).one(":dialogclosed", function () {
+        $.wiki(onCloseString.trim());
+      });
+    }
+
+    Dialog.open();
   },
 });
 
--- test/dialog-element-macro.test.ts
+++ test/dialog-element-macro.test.ts
@@ -2,9 +2,12 @@ import { Selector } from "testcafe";
 
 fixture.page(`../dist_test/index.html`)(`Dialog Element Macro`);
 
-test(`testcafe works`, async (t: TestController): Promise<void> => {
+test(`it renders a dialog element`, async (t: TestController): Promise<void> => {
   await t.setNativeDialogHandler((type, text) => {
     throw new Error(text);
   });
-  await t.click(Selector(".passage button").withText("Open simple dialog"));
+  await t
+    .click(Selector(".passage button").withText("Open simple dialog"))
+    .expect(Selector(".passage dialog").exists)
+    .ok();
 });
