09f84f4de5aa81b03b79bf4eea1cc2c24abb6884
Bawdy Ink Slinger <BawdyInkSlinger@gmail.com>
2023-07-07 19:32:26 -0700

    Additional project set up

--- test/dialog-element-macro.test.ts
+++ test/dialog-element-macro.test.ts
@@ -8,14 +8,14 @@ test(`it renders a simple dialog element`, async (t: TestController): Promise<vo
   });
   await t.click(Selector(".passage button").withText("Open simple dialog"));
 
-  await assertOpenSimpleDialog(t);
+  await assertOpenSimpleDialog(t, 1);
   await t
     .click(Selector(".passage button").withText("Close"))
-    .expect(Selector(".passage .macro-dialogelement.dialog-element").visible)
+    .expect(Selector(".passage .macro-dialogelement.dialog-element").exists)
     .notOk();
-  // it can open again
+  // it re-creates the dialog when you click the button
   await t.click(Selector(".passage button").withText("Open simple dialog"));
-  await assertOpenSimpleDialog(t);
+  await assertOpenSimpleDialog(t, 2);
 });
 
 test(`it renders on top of Dialog API`, async (t: TestController): Promise<void> => {
@@ -26,13 +26,17 @@ test(`it renders on top of Dialog API`, async (t: TestController): Promise<void>
   await t.click(
     Selector("#ui-dialog-body button").withText("Open simple dialog")
   );
-  await assertOpenSimpleDialog(t);
+  await assertOpenSimpleDialog(t, 1);
 });
 
+//todo: you can't interact with elements outside of the modal
 //todo: click outside of both dialogs, only the dialog element disappears
 //todo: the dialog element macro comes with a built in close button
 
-async function assertOpenSimpleDialog(t: TestController) {
+async function assertOpenSimpleDialog(
+  t: TestController,
+  expectedDialogCount: number
+) {
   await t
     .expect(Selector(".passage .macro-dialogelement.dialog-element").count)
     .eql(1)
@@ -47,14 +51,8 @@ async function assertOpenSimpleDialog(t: TestController) {
     .eql(1)
     .expect(
       Selector(
-        ".passage .macro-dialogelement .dialog-element-body.class-a.class-b.class-c.class-d span"
+        `.passage .macro-dialogelement .dialog-element-body.class-a.class-b.class-c.class-d.dialog-number-${expectedDialogCount} span`
       ).innerText
     )
-    .eql("My content")
-    .expect(
-      Selector(
-        ".passage .macro-dialogelement .dialog-element-body.class-a.class-b.class-c.class-d span"
-      ).count
-    )
-    .eql(1);
+    .eql("My content");
 }
--- watch.sh
+++ watch.sh
@@ -3,7 +3,6 @@
 echo Executing: `basename "$0"`
 set -uxe
 
-# kill child processes when this process dies: https://stackoverflow.com/a/2173421/61624
 # "trap - SIGINT SIGTERM EXIT" stops trapping these signals to prevent recursion
 trap "trap - SIGINT SIGTERM EXIT; ./watch-cleanup.sh; exit" SIGINT SIGTERM EXIT
 
@@ -14,5 +13,5 @@ npx concurrently --names "sass,tweego,ts" "npm run watch-sass-files" "npm run wa
 
 echo -n "$$" >> watch.sh.pids
 
-# wait forever: https://unix.stackexchange.com/a/42905/101311
+# wait forever
 tail -f /dev/null
