03382f3bac1de6108969b2832cfb8351f9ce5464
Bawdy Ink Slinger <BawdyInkSlinger@gmail.com>
2023-07-17 13:35:09 -0700

    extracted test/fixture for recreate-dialog-element

--- test/dialog-element-macro.test.ts
+++ test/dialog-element-macro.test.ts
@@ -7,40 +7,6 @@ import {
 
 fixture.page(`../dist_test/index.html`)(`Dialog Element Macro`);
 
-test(`can recreate dialog element macros`, async (t: TestController): Promise<void> => {
-  await t.setNativeDialogHandler((type, text) => {
-    throw new Error(text);
-  });
-
-  await t
-    .click(
-      Selector('.passage button').withText(
-        'Test can recreate dialog element macros'
-      )
-    )
-    .click(Selector('.passage button').withText('Open a dialog'))
-    .expect(dialogElementCount())
-    .eql(1);
-  await expectDialogElement({
-    exactTitle: 'My Title',
-    bodyText: 'My content',
-    customClassNames: ['dialog-number-1'],
-  });
-  await t
-    .click(Selector('.passage button.dialog-element-close'))
-    .expect(Selector('.passage .macro-dialogelement.dialog-element').exists)
-    .notOk()
-    // it re-creates the dialog when you click the button
-    .click(Selector('.passage button').withText('Open a dialog'))
-    .expect(dialogElementCount())
-    .eql(1);
-  await expectDialogElement({
-    exactTitle: 'My Title',
-    bodyText: 'My content',
-    customClassNames: ['dialog-number-2'],
-  });
-});
-
 test(`can stack multiple dialogs and close them, top to bottom, while triggering onopen and onclose`, async (t: TestController): Promise<void> => {
   await t.setNativeDialogHandler((type, text) => {
     throw new Error(text);
---
+++ test/recreate-dialog-element-macros.test.ts
@@ -0,0 +1,41 @@
+import { Selector, t } from 'testcafe';
+import {
+  dialogElementCount,
+  expectDialogElement,
+} from './dialog-element-testcafe-utils';
+
+fixture.page(`../dist_test/index.html`)(`Dialog Element Macro`);
+
+test(`can recreate dialog element macros`, async (t: TestController): Promise<void> => {
+  await t.setNativeDialogHandler((type, text) => {
+    throw new Error(text);
+  });
+
+  await t
+    .click(
+      Selector('.passage button').withText(
+        'Test can recreate dialog element macros'
+      )
+    )
+    .click(Selector('.passage button').withText('Open a dialog'))
+    .expect(dialogElementCount())
+    .eql(1);
+  await expectDialogElement({
+    exactTitle: 'My Title',
+    bodyText: 'My content',
+    customClassNames: ['dialog-number-1'],
+  });
+  await t
+    .click(Selector('.passage button.dialog-element-close'))
+    .expect(Selector('.passage .macro-dialogelement.dialog-element').exists)
+    .notOk()
+    // it re-creates the dialog when you click the button
+    .click(Selector('.passage button').withText('Open a dialog'))
+    .expect(dialogElementCount())
+    .eql(1);
+  await expectDialogElement({
+    exactTitle: 'My Title',
+    bodyText: 'My content',
+    customClassNames: ['dialog-number-2'],
+  });
+});
