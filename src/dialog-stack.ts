880c4bcc8deb1ac55e1ed8b04d970f4c64d3fe5f
Bawdy Ink Slinger <BawdyInkSlinger@gmail.com>
2023-07-20 19:21:50 -0700

    Implemented changes from Issue #1
    - https://github.com/BawdyInkSlinger/dialog-element-macro/issues/1

--- src/dialog-stack.ts
+++ src/dialog-stack.ts
@@ -1,22 +1,20 @@
 export const DialogElementMacroStack = {
   push: function ($dialog: JQuery<HTMLDialogElement>) {
-    if (this.dialogStackExists()) {
-      State.temporary.dialog_element_macro_stack = [];
+    if (!dialogStackExists()) {
+      State.temporary[`#dialog_element_macro_stack`] = [];
     }
-    State.temporary.dialog_element_macro_stack.push($dialog);
+    State.temporary[`#dialog_element_macro_stack`].push($dialog);
   },
   pop: function (): JQuery<HTMLDialogElement> {
-    if (this.dialogStackExists()) {
+    if (!dialogStackExists()) {
       throw new Error(
         `Trying to pop the Dialog Element Macro's dialog stack, but it is undefined.`
       );
     }
-    return State.temporary.dialog_element_macro_stack.pop();
-  },
-  dialogStackExists: function (): boolean {
-    return (
-      State.temporary?.dialog_element_macro_stack === null ||
-      State.temporary?.dialog_element_macro_stack === undefined
-    );
+    return State.temporary[`#dialog_element_macro_stack`].pop();
   },
 };
+
+function dialogStackExists(): boolean {
+  return State.temporary[`#dialog_element_macro_stack`] != null;
+}
